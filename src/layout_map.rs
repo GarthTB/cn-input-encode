1cfe1dcb2ba496f9985ff5f2df1f7c590f5bd4c5
Garth TB | 天卜 <g-art-h@outlook.com>
2026-04-22 00:30:44 +0800

    添加键盘布局配置模型

---
+++ src/layout_map.rs
@@ -0,0 +1,35 @@
+pub(crate) struct LayoutMap {
+    map: rustc_hash::FxHashMap<char, u8>,
+}
+
+impl LayoutMap {
+    pub(crate) fn new(layout: &[String]) -> Result<Self, String> {
+        if layout.len() != 14 {
+            return Err("布局配置非14项".into());
+        }
+        let mut map = rustc_hash::FxHashMap::with_capacity_and_hasher(50, Default::default());
+        let (fingers, rows) = layout.split_at(9);
+        for (i, f) in (1u8..10).zip(fingers) {
+            let v = i << 4;
+            for c in f.chars() {
+                if map.insert(c, v).is_some() {
+                    return Err(format!("键'{c}'手指冲突"));
+                }
+            }
+        }
+        for (i, r) in (1u8..6).zip(rows) {
+            for c in r.chars() {
+                if *map.entry(c).and_modify(|v| *v += i).or_insert(i) & 0xF != i {
+                    return Err(format!("键'{c}'排冲突"));
+                }
+            }
+        }
+        Ok(Self { map })
+    }
+
+    #[inline]
+    pub(crate) fn get(&self, c: char) -> (u8, u8) {
+        let v = self.map.get(&c).unwrap_or(&0);
+        (v >> 4, v & 0xF)
+    }
+}
