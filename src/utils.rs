cc2af53ba701b0b32de0c0b35d86c55d163930e0
Garth TB | 天卜 <g-art-h@outlook.com>
2026-04-25 01:04:05 +0800

    添加分块处理文件的工具方法

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,30 @@
-#[inline]
-pub(crate) fn pack(a: char, b: char) -> u64 {
-    (a as u64) << 32 | (b as u64)
-}
+use std::io::Read;
 
-#[inline]
 pub(crate) fn fx_hash_map_with_capacity<K, V>(c: usize) -> rustc_hash::FxHashMap<K, V> {
     rustc_hash::FxHashMap::with_capacity_and_hasher(c, Default::default())
+}
+
+pub(crate) fn for_each_chunk<F: FnMut(&str)>(path: &str, mut f: F) -> crate::DynResult<()> {
+    let mut buf = vec![0; crate::CHUNK_SIZE].into_boxed_slice();
+    let mut len = 0;
+    let mut file = std::fs::File::open(path)?;
+    loop {
+        match file.read(&mut buf[len..]) {
+            Err(e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e.into()),
+            Ok(n) if n > 0 => len += n,
+            _ => return Ok(()),
+        }
+        let s = match std::str::from_utf8(&buf[..len]) {
+            Err(e) if e.error_len().is_some() => return Err(e.into()),
+            Err(e) => unsafe { std::str::from_utf8_unchecked(&buf[..e.valid_up_to()]) },
+            Ok(s) => s,
+        };
+        f(s);
+        let s_len = s.len();
+        if s_len > 0 && s_len < len {
+            buf.copy_within(s_len..len, 0);
+        }
+        len -= s_len;
+    }
 }
