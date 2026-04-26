9c44acc9f1e02c5960f44cf5991f5102e57ca127
Garth TB | 天卜 <g-art-h@outlook.com>
2026-04-26 13:30:02 +0800

    完成主函数逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,59 @@
-mod chunk_reader;
+use std::{error, fs, io, io::Write, path::Path};
+
 mod config;
 mod cost_map;
+mod dp_encoder;
 mod layout_map;
 mod trie_dict;
+mod usage_analyzer;
 mod utils;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub(crate) const CHUNK_SIZE: usize = 1 << 18; // 爆栈风险
+pub(crate) type DynResult<T> = Result<T, Box<dyn error::Error>>;
+
+fn main() -> DynResult<()> {
     println!(
         "cn-input-encode 1.0.0 (20260501)\n\
         作者：Garth TB | 天卜 <g-art-h@outlook.com>\n\
         仓库：https://github.com/GarthTB/cn-input-encode\n\
         加载配置..."
     );
-    Ok(())
+    let config = config::Config::load()?;
+    let mut encoder = dp_encoder::Encoder::new(&config.dict);
+    let mut oo = fs::OpenOptions::new();
+    let opener = oo.append(true).create(true);
+    println!("...加载完成");
+
+    for i_path in config.text {
+        let p = Path::new(&i_path);
+        let stem = p.file_stem().unwrap_or_default().to_string_lossy();
+        let dir = p.parent().unwrap_or(Path::new("."));
+        let mut o_path = dir.join(format!("{stem}-code.txt"));
+        let mut n = 2u32;
+        while fs::exists(&o_path)? {
+            o_path = dir.join(format!("{stem}-code-{n}.txt"));
+            n += 1;
+        }
+        let o_path = o_path.to_string_lossy().to_string();
+        let mut o_file = opener.open(&o_path)?;
+
+        println!("开始编码文件'{i_path}'...");
+        encoder.reset();
+        utils::for_each_chunk(&i_path, |s| {
+            let (from, to) = encoder.shrink_and_feed(s);
+            print!("\r第{from}-{to}字...");
+            io::stdout().flush()?;
+            encoder.proc_chunk(&config.costs);
+            encoder.join_and_append(&mut o_file)
+        })?;
+        let (t_len, c_len, cost) = encoder.proc_end(&mut o_file)?;
+        println!("完成，共{t_len}字");
+
+        println!("分析数据...");
+        let report = usage_analyzer::analyze(&o_path, t_len, c_len, cost, &config.layout)?;
+        o_file.write_all(&report)?;
+        println!("...完成，结果已写入'{o_path}'");
+    }
+
+    Ok(println!("全部完成，程序结束"))
 }
